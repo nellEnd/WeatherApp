c2164300f6da2647f34024556d411ce31742fda8
NellyE <119869318+NellyEndler@users.noreply.github.com>
2024-01-16 11:12:24 +0100

    Added Tailwindcss and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
-
 import './App.css'
+import { Axios } from 'axios';
+import WeatherApp from './weather_app/WeatherApp';
 
 function App() {
+  return(
+    <div className='w-full h-full relative'>
+      <div className='text-center p-4'>
+        <input type="text" />
+
+      </div>
+    </div>
+    );
 
 }
 
